50da1d53ef3f132efafeef3684de36cea3ba686e
tugbakunkul23 <tugbakunkul7@gmail.com>
2025-08-05 18:12:18 +0300

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turkcell_proje_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya");
+
+            Console.Read();  //Read --> enter a bastığımda ekran kapanır.
+
+        }
+    }
+}
