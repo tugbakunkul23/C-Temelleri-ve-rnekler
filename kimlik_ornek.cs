9dff4d370d78f3aa11c7380f966452eca1f36653
tugbakunkul23 <tugbakunkul7@gmail.com>
2025-08-05 18:13:34 +0300

    Create kimlik_ornek.cs

---
+++ kimlik_ornek.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kimlik_ornek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Kimlik *****");
+            Console.WriteLine();
+            Console.WriteLine("Tuğba");   //write yanyana yazdırır ,writeline bi alt satıra geçirir.imleci aşağıya odaklar
+            Console.WriteLine("KÜNKÜL");
+            Console.WriteLine("Fırat Üniversitesi");
+            Console.WriteLine("Elazığ");
+            Console.WriteLine();
+            Console.Write("***** Kimlik *****");
+
+            Console.Read();
+        }
+    }
+}
